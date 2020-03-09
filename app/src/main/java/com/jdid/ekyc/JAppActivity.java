28852cf5ee45457d88de8c3b659fb10d333f0cb5
poobest <poobest.pooh@gmail.com>
2020-03-09 15:39:56 +0700

    release 1.0.16 (check dopa)

--- app/src/main/java/com/jdid/ekyc/JAppActivity.java
+++ app/src/main/java/com/jdid/ekyc/JAppActivity.java
@@ -101,8 +101,8 @@ public class JAppActivity extends JCompatActivity {
 
     private static final String TAG = "JAppActivity";
 
-    public static final String APP_VERSION = "release 1.0.15";
-    public static final String APP_DATE_UPDATE = "08/03/63";
+    public static final String APP_VERSION = "release 1.0.16";
+    public static final String APP_DATE_UPDATE = "09/03/63";
 
     private static final int PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_CODE = 1001;
